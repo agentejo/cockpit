7868438496ddaf642e9f479313dda1b602bc5ddc
Artur <artur@agentejo.com>
2013-12-22 13:32:05 +0100

    wysiwyg directive added

---
+++ assets/angular/directives/wysiwyg.js
@@ -0,0 +1,82 @@
+/**
+ * Binds a TinyMCE widget to <textarea> elements.
+ */
+
+(function($){
+  angular.module('cockpit.directives').directive("wysiwyg", function($timeout){
+
+      var defaultConfig = {}, generatedIds  = 0;
+      
+      return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ngModel) {
+          var expression, options, tinyInstance,
+            updateView = function () {
+              ngModel.$setViewValue(elm.val());
+              if (!scope.$root.$$phase) {
+                scope.$apply();
+              }
+            };
+          // generate an ID if not present
+          if (!attrs.id) {
+            attrs.$set('id', 'wysiwyg' + generatedIds++);
+          }
+
+          if (attrs.wysiwyg) {
+            expression = scope.$eval(attrs.wysiwyg);
+          } else {
+            expression = {};
+          }
+          options = {
+            // Update model when calling setContent (such as from the source editor popup)
+            setup: function (ed) {
+              var args;
+              ed.on('init', function(args) {
+                ngModel.$render();
+              });
+              // Update model on button click
+              ed.on('ExecCommand', function (e) {
+                ed.save();
+                updateView();
+              });
+              // Update model on keypress
+              ed.on('KeyUp', function (e) {
+                ed.save();
+                updateView();
+              });
+              // Update model on change, i.e. copy/pasted text, plugins altering content
+              ed.on('SetContent', function (e) {
+                if(!e.initial){
+                  ed.save();
+                  updateView();
+                }
+              });
+              if (expression.setup) {
+                scope.$eval(expression.setup);
+                delete expression.setup;
+              }
+            },
+            mode: 'exact',
+            elements: attrs.id
+          };
+          
+          // extend options with initial defaultConfig and options from directive attribute value
+          angular.extend(options, defaultConfig, expression);
+          setTimeout(function () {
+            tinymce.init(options);
+          });
+
+
+          ngModel.$render = function() {
+            if (!tinyInstance) {
+              tinyInstance = tinymce.get(attrs.id);
+            }
+            if (tinyInstance) {
+              tinyInstance.setContent(ngModel.$viewValue || '');
+            }
+          };
+        }
+      };
+    }]);
+
+})(jQuery);
