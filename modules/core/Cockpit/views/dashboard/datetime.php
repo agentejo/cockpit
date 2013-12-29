7fad715367476a0fce96100d5ed10f04ba77f495
Artur <artur@agentejo.com>
2013-12-29 19:40:18 +0100

    minor ui improvements

--- modules/core/Cockpit/views/dashboard/datetime.php
+++ modules/core/Cockpit/views/dashboard/datetime.php
@@ -19,8 +19,8 @@
 <hr>
 
 <div class="date-widget-account">
-    <a href="@route('/accounts/account')" class="uk-clearfix">
-        <img class="uk-rounded uk-float-left uk-margin-right" src="http://www.gravatar.com/avatar/{{ md5($app['user']['email']) }}?d=mm&s=40" width="40" height="40" alt="avatar">
+    <a class="uk-display-block" href="@route('/accounts/account')" class="uk-clearfix" title="Edit account settings" data-uk-tooltip="{pos:'bottom-left', offset:10}">
+        <img class="uk-rounded uk-float-left uk-margin-right" src="http://www.gravatar.com/avatar/{{ md5($app['user']['email']) }}?d=mm&s=35" width="35" height="35" alt="avatar">
         <div class="uk-text-truncate"><strong>{{ $app["user"]["user"] }}</strong></div>
         <div class="uk-text-small uk-text-muted uk-text-truncate">{{ (isset($app["user"]["email"]) ? $app["user"]["email"] : 'no email') }}</div>
     </a>
@@ -37,7 +37,7 @@
     }
     .date-widget-account { 
         opacity: 0.3; 
-        transition: opacity 0.3s ease-in-out;
+        transition: opacity 0.2s ease-in-out;
     }
     .date-widget-account:hover { opacity: 1; }
 </style>
