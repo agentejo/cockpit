226580b7c111bd01aa587bed3550ba9a228649f4
Artur Heinze <artur@yootheme.com>
2014-02-13 14:27:34 +0100

    cleanup

--- install/index.php
+++ install/index.php
@@ -44,7 +44,6 @@ $account = [
     "i18n"     => "en"
 ];
 
-$app->getCollection("cockpit/accounts")->insert($account);
-
+$app->db->insert("cockpit/accounts", $account);
 
 include(__DIR__."/success.php");
