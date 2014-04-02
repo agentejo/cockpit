9d5ec3167380eeec4d73d03484d26eb14547f3cd
Artur <artur@agentejo.com>
2014-04-02 22:27:37 +0200

    markdown area field directive updated

--- assets/angular/directives/markdownarea.js
+++ assets/angular/directives/markdownarea.js
@@ -5,6 +5,21 @@
 (function($){
 
 
+    var template = $.UIkit.Utils.template([
+        '<div id="{{{ uid }}}" class="uk-overlay uk-display-block">',
+            '{{{ img }}}',
+            '<div class="uk-overlay-area">',
+                '<div class="uk-overlay-area-content">',
+                    '<div><span class="uk-badge">{{{ alt }}}</span></div>',
+                    '<div class="uk-button-group uk-margin-top">',
+                        '<button class="uk-button uk-button-primary js-config" type="button" title="Pick image"><i class="uk-icon-hand-o-up"></i></button>',
+                        '<button class="uk-button uk-button-danger js-remove" type="button" title="Remove image"><i class="uk-icon-trash-o"></i></button>',
+                    '</div>',
+                '</div>',
+            '</div>',
+        '</div>'
+    ].join(""));
+
     function autocomplete(cm) {
         var doc = cm.getDoc(),
             cur = cm.getCursor(),
@@ -25,6 +40,40 @@
         }
     };
 
+    $.UIkit.markdownarea.addPlugin('htmlimages', /<img(.+?)>/gim, function(marker) {
+
+        var img, attrs = {"src":"", "alt":""};
+
+        marker.found[0].match(/(\S+)=["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.)["']?/g).forEach(function(attr){
+            var parts = attr.replace(/('|")/g, '').split("=");
+            attrs[parts[0]] = parts[1];
+        });
+
+        if (attrs.src && 'http://'!==attrs.src.trim()) {
+          img = '<img src="'+attrs.src+'" alt="'+attrs.alt+'">';
+        } else {
+          img = [
+            '<div class="uk-placeholder uk-placeholder-large uk-text-center uk-vertical-align">',
+              '<div class="uk-vertical-align-middle"><i class="uk-icon-picture-o"></i></div>',
+            '</div>'
+          ].join("");
+        }
+
+        var replacement = template({"img":img, "uid":marker.uid, "alt": (attrs.alt || 'Image') });
+
+        marker.area.preview.on('click', '#' + marker.uid + ' .js-config', function () {
+            new PathPicker(function(path){
+                marker.replace('<img src="'+path.replace('site:', COCKPIT_SITE_BASE_URL)+'" alt="'+attrs.alt+'">');
+            }, "*.(jpg|png|gif)");
+        });
+
+        marker.area.preview.on('click', '#' + marker.uid + ' .js-remove', function () {
+            marker.replace('');
+        });
+
+        return replacement;
+    });
+
     $.UIkit.markdownarea.addPlugin('images', /(?:\{<(.*?)>\})?!(?:\[([^\n\]]*)\])(?:\(([^\n\]]*)\))?$/gim, function (marker) {
 
         var img;
@@ -39,20 +88,7 @@
           ].join("");
         }
 
-        var replacement = [
-            '<div id="'+marker.uid+'" class="uk-overlay uk-display-block">',
-                img,
-                '<div class="uk-overlay-area">',
-                    '<div class="uk-overlay-area-content">',
-                        '<div><span class="uk-badge">'+(marker.found[2] || 'Image')+'</span></div>',
-                        '<div class="uk-button-group uk-margin-top">',
-                            '<button class="uk-button uk-button-primary js-config" type="button" title="Pick image"><i class="uk-icon-hand-o-up"></i></button>',
-                            '<button class="uk-button uk-button-danger js-remove" type="button" title="Remove image"><i class="uk-icon-trash-o"></i></button>',
-                        '</div>',
-                    '</div>',
-                '</div>',
-            '</div>'
-        ].join("");
+        var replacement = template({"img":img, "uid":marker.uid, "alt": (marker.found[2] || 'Image') });
 
         marker.area.preview.on('click', '#' + marker.uid + ' .js-config', function () {
             new PathPicker(function(path){
