5aa95fce21b73fc7ffb972b6d4bf9822639df9e7
Russell Hutson <russell.hutson@exertismicro-p.co.uk>
2014-06-04 18:51:40 +0000

    Collection REST API changed to support 'approved' option to only return entries where all approval fields are true

--- modules/core/Collections/Controller/Api.php
+++ modules/core/Collections/Controller/Api.php
@@ -25,6 +25,26 @@ class Api extends \Cockpit\Controller {
         return json_encode($docs->toArray());
     }
 
+      public function findApproved(){
+
+        $options = [];
+
+        if($filter = $this->param("filter", null)) $options["filter"] = $filter;
+        if($limit  = $this->param("limit", null))  $options["limit"] = $limit;
+        if($sort   = $this->param("sort", null))   $options["sort"] = $sort;
+        if($skip   = $this->param("skip", null))   $options["skip"] = $skip;
+
+        $docs = $this->app->db->find("common/collections", $options);
+
+        if(count($docs) && $this->param("extended", false)){
+            foreach ($docs as &$doc) {
+                $doc["count"] = $this->app->module("collections")->collectionById($doc["_id"])->count();
+            }
+        }
+
+        return json_encode($docs->toArray());
+    } // findApproved
+
     public function findOne(){
 
         $doc = $this->app->db->findOne("common/collections", $this->param("filter", []));
--- modules/core/Collections/Controller/RestApi.php
+++ modules/core/Collections/Controller/RestApi.php
@@ -4,6 +4,12 @@ namespace Collections\Controller;
 
 class RestApi extends \LimeExtra\Controller {
 
+      /**
+       * RCH 20140604
+       * Check Collection definition for any 'approval' fields.
+       * if any of those fields are not set to true then the entry will not be returned
+       * Need ideally to iterate through versions of the entry to find the latest fully approved version
+       */
     public function get($collection=null) {
 
         if(!$collection) {
@@ -27,6 +33,23 @@ class RestApi extends \LimeExtra\Controller {
             if($limit  = $this->param("limit", null))  $options["limit"] = $limit;
             if($sort   = $this->param("sort", null))   $options["sort"] = $sort;
             if($skip   = $this->param("skip", null))   $options["skip"] = $skip;
+
+            // RCH 20140604
+            if($approved = $this->param("approved", null)) {
+              // Find all the approval fields for this collection
+              //return('collection = '.print_r($collection,true));
+              $approvedOnly = array();
+              foreach($collection['fields'] as $field) {
+                // is this field an approval field?
+                if ($field['type']=='approval')
+                  $approvedOnly[$field['name']]=1; // only include entries where this field is true
+              }
+              if (!empty($options["filter"]))
+                $options["filter"] = array_merge($options["filter"], $approvedOnly);
+              else 
+                $options["filter"] = $approvedOnly;
+            }
+            //return('options = '.print_r($options,true));
 
             $entries = $this->app->db->find("collections/{$col}", $options);
         }
