527022e0e77b02fe59f8a9bbf9b5f6f4cad9f555
Artur <artur@agentejo.com>
2014-08-11 00:44:53 +0200

    thumbnails api function added

--- modules/core/Mediamanager/Controller/RestApi.php
+++ modules/core/Mediamanager/Controller/RestApi.php
@@ -4,6 +4,10 @@ namespace Mediamanager\Controller;
 
 class RestApi extends \LimeExtra\Controller {
 
+
+    /*
+        deprecated
+    */
     public function thumbnail($image, $width = 50, $height = 50) {
 
         $image = base64_decode($image);
@@ -11,6 +15,28 @@ class RestApi extends \LimeExtra\Controller {
         $imgurl = $this->module("mediamanager")->thumbnail($image, $width, $height);
 
         return $imgurl;
+    }
+
+
+    public function thumbnails() {
+
+        $images  = $this->param('images', []);
+        $width   = $this->param('w', 50);
+        $height  = $this->param('h', false);
+
+        $options = [
+            "quality"     => $this->param('q', 100),
+            "base64"      => $this->param('base64', false),
+            "mode"        => $this->param('m', 'crop')
+        ];
+
+        $urls = [];
+
+        foreach ($images as $image) {
+            $urls[$image] = $this->module("mediamanager")->thumbnail($image, $width, $height, $options);
+        }
+
+        return json_encode($urls);
     }
 
 }
