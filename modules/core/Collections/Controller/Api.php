8ae5ee7e89c8d6fa4226fc019ba4099df1644c20
Piotr Konieczny <hello@piotr.cz>
2015-07-17 15:36:03 +0200

    Unique fields bugfix

--- modules/core/Collections/Controller/Api.php
+++ modules/core/Collections/Controller/Api.php
@@ -176,21 +176,28 @@ class Api extends \Cockpit\Controller {
             $col = "collection".$collection["_id"];
 
             // Check for uniqueeness of fields set as unique
-            foreach ($collection['fields'] as $fieldDefinition) {
-                
-                $fieldName = $fieldDefinition['name'];
+            foreach ($collection["fields"] as $fieldDefinition) {
 
-                if (
-                    $fieldDefinition['unique'] && 
-                    isset($entry[$fieldName]) && 
-                    $this->app->db->findOne("collections/{$col}", [$fieldName => $entry[$fieldName]])
-                ) {
-                    $this->app->response->status = 409;
+                $fieldName = $fieldDefinition["name"] . "_slug";
 
-                    return sprintf(
-                        $this->app->helper('i18n')->get("There is already an entry in this collection with this slug for field '%s'."),
-                        $fieldName
-                    );
+                if ($fieldDefinition["slug"] && $fieldDefinition["unique"] && isset($entry[$fieldName])) {
+
+                    $collision = $this->app->db->findOne("collections/{$col}", [$fieldName => $entry[$fieldName]]);
+
+                    if (!$collision) {
+                        continue;
+                    }
+
+                    // New and colliding or other entry
+                    if (!isset($entry["_id"]) || $entry["_id"] != $collision["_id"]) {
+
+                        $this->app->response->status = 409;
+
+                        return sprintf(
+                            $this->app->helper("i18n")->get("There is already an entry in this collection with this slug for field '%s'."),
+                            $fieldDefinition["label"]
+                        );
+                    }
                 }
             }
 
