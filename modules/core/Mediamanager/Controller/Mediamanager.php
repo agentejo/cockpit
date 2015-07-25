72c4aad41fe5f11dd95408da74768a57ab36abd2
Charles Willgren <charkes@gmail.com>
2015-07-25 06:05:36 -0500

    Auto-orient cases attached
    This code was present during the commit.  Hiccup in GitHub apparently.

--- modules/core/Mediamanager/Controller/Mediamanager.php
+++ modules/core/Mediamanager/Controller/Mediamanager.php
@@ -110,8 +110,35 @@ class Mediamanager extends \Cockpit\Controller {
                     $exif = function_exists('exif_read_data') ? @exif_read_data($savepath) : [];
 
                     if (isset($exif['Orientation'])) {
+                        if ($exif['Orientation'] >= 2 && $exif['Orientation'] <= 8) {
+                            $img = $this("image")->take($savepath);
 
+                            switch ($exif['Orientation']) {
+                                case 2:
+                                    $img->flipHorizontally();
+                                    break;
+                                case 3:
+                                    $img->rotate(-180);
+                                    break;
+                                case 4:
+                                    $img->flipVertically();
+                                    break;
+                                case 5:
+                                    $img->flipVertically()->rotate(90);
+                                    break;
+                                case 6:
+                                    $img->rotate(90);
+                                    break;
+                                case 7:
+                                    $img->flipHorizontally()->rotate(90);
+                                    break;
+                                case 8:
+                                    $img->rotate(-90);
+                                    break;
+                            }
 
+                            $img->save($savepath, 100);
+                        }
                     }
 
                     $uploaded[] = $files['name'][$i];
