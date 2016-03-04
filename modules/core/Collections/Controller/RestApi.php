610fca3f8411a8eba6b832b12ab438a4f61c0c1b
Gryphon Myers <gryphon.myers@2k.com>
2016-03-04 11:44:04 -0800

    added denormalization parameter to rest api

--- modules/core/Collections/Controller/RestApi.php
+++ modules/core/Collections/Controller/RestApi.php
@@ -3,6 +3,40 @@
 namespace Collections\Controller;
 
 class RestApi extends \LimeExtra\Controller {
+    private function get_item_from_hash($propName, $val) {
+        static $cache = [];
+        if (substr($propName, 0, 1) != "_" && preg_match('/^[0-9a-fA-F]{24}$/', $val)) {
+            if (!array_key_exists($val, $cache)) {
+                if (!array_key_exists($propName, $cache)) {
+                    $cache[$propName] = $this->app->db->findOne("common/collections",["name"=>$propName]);
+                    $cache[$cache[$propName]["_id"]] = $cache[$propName];
+                }
+                $cache[$val] = $this->denormalize_entry($this->app->db->findOneById("collections/collection".$cache[$propName]["_id"], $val));
+            }
+            if ($cache[$val]) {
+                return $cache[$val];
+            }
+        }
+        return $val;
+    }
+
+    private function denormalize_entry($entry) {
+        if ($entry) {
+            foreach($entry as $propName => $val) {
+                // One gotcha of this function - $propName (and thus field name of collection link) must match the name of the collection a given entry belongs to. Otherwise denormalization will not know where to look for item hash and fail.
+                if(is_array($val)) {
+                    $result = $val;
+                    foreach($val as $key=>$obj_id) {
+                        $result[$key] = $this->get_item_from_hash($propName, $obj_id);
+                    }
+                    $entry[$propName] = $result;
+                } else {
+                    $entry[$propName] = $this->get_item_from_hash($propName, $val);
+                }
+            }
+        }
+        return $entry;
+    }
 
     public function get($collection=null) {
 
@@ -10,7 +44,7 @@ class RestApi extends \LimeExtra\Controller {
             return false;
         }
 
-        $collection = $this->app->db->findOne("common/collections", ["name"=>$collection]);
+        $collection = $this->app->db->findOne("common/collections",  ["name"=>$collection]);
 
         if (!$collection) {
             return false;
@@ -29,6 +63,13 @@ class RestApi extends \LimeExtra\Controller {
             if ($skip   = $this->param("skip", null))   $options["skip"] = $skip;
 
             $entries = $this->app->db->find("collections/{$col}", $options);
+        }
+
+
+        if ($this->param("denormalize")) {
+            foreach($entries as &$entry) {
+                $entry = $this->denormalize_entry($entry);
+            }
         }
 
         return json_encode($entries->toArray());
