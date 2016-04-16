fe4de80491f2930bb8bd48bd042c6efc4c18afde
Artur <artur@agentejo.com>
2016-04-16 22:38:09 +0200

    quicknav icon size updated

--- modules/core/Cockpit/views/layouts/app.php
+++ modules/core/Cockpit/views/layouts/app.php
@@ -67,24 +67,24 @@
 
                                         <ul class="uk-nav uk-nav-side uk-nav-dropdown app-nav">
 
-                                            <li class="{{ $app['route'] == '/cockpit/dashboard' ? 'uk-active':'' }}"><a href="@route('/cockpit/dashboard')"><img class="uk-margin-small-right inherit-color" src="@base('assets:app/media/icons/dashboard.svg')" width="32" height="32" data-uk-svg alt="assets" /> @lang('Dashboard')</a></li>
+                                            <li class="{{ $app['route'] == '/cockpit/dashboard' ? 'uk-active':'' }}"><a href="@route('/cockpit/dashboard')"><img class="uk-margin-small-right inherit-color" src="@base('assets:app/media/icons/dashboard.svg')" width="30" height="30" data-uk-svg alt="assets" /> @lang('Dashboard')</a></li>
 
-                                            <li class="{{ strpos($app['route'],'/assetsmanager')===0 ? 'uk-active':'' }}"><a href="@route('/assetsmanager')"><img class="uk-margin-small-right inherit-color" src="@base('assets:app/media/icons/assets.svg')" width="32" height="32" data-uk-svg alt="assets" /> @lang('Assets')</a></li>
+                                            <li class="{{ strpos($app['route'],'/assetsmanager')===0 ? 'uk-active':'' }}"><a href="@route('/assetsmanager')"><img class="uk-margin-small-right inherit-color" src="@base('assets:app/media/icons/assets.svg')" width="30" height="30" data-uk-svg alt="assets" /> @lang('Assets')</a></li>
 
                                             <li class="uk-nav-divider"></li>
 
                                             @hasaccess?('cockpit', 'manage.accounts')
-                                            <li class="{{ strpos($app['route'],'/accounts')===0 ? 'uk-active':'' }}"><a href="@route('/accounts')"><img class="uk-margin-small-right inherit-color" src="@base('assets:app/media/icons/accounts.svg')" width="32" height="32" data-uk-svg alt="assets" /> @lang('Accounts')</a></li>
+                                            <li class="{{ strpos($app['route'],'/accounts')===0 ? 'uk-active':'' }}"><a href="@route('/accounts')"><img class="uk-margin-small-right inherit-color" src="@base('assets:app/media/icons/accounts.svg')" width="30" height="30" data-uk-svg alt="assets" /> @lang('Accounts')</a></li>
                                             @end
 
                                             <li class="uk-nav-divider"></li>
 
                                             @hasaccess?('cockpit', 'manage.media')
-                                            <li class="{{ strpos($app['route'],'/finder')===0 ? 'uk-active':'' }}"><a href="@route('/finder')"><img class="uk-margin-small-right inherit-color" src="@base('assets:app/media/icons/finder.svg')" width="32" height="32" data-uk-svg alt="assets" /> @lang('Finder')</a></li>
+                                            <li class="{{ strpos($app['route'],'/finder')===0 ? 'uk-active':'' }}"><a href="@route('/finder')"><img class="uk-margin-small-right inherit-color" src="@base('assets:app/media/icons/finder.svg')" width="30" height="30" data-uk-svg alt="assets" /> @lang('Finder')</a></li>
                                             @end
 
                                             @hasaccess?('cockpit', 'manage.settings')
-                                            <li class="{{ strpos($app['route'],'/settings')===0 ? 'uk-active':'' }}"><a href="@route('/settings')"><img class="uk-margin-small-right inherit-color" src="@base('assets:app/media/icons/settings.svg')" width="32" height="32" data-uk-svg alt="assets" /> @lang('Settings')</a></li>
+                                            <li class="{{ strpos($app['route'],'/settings')===0 ? 'uk-active':'' }}"><a href="@route('/settings')"><img class="uk-margin-small-right inherit-color" src="@base('assets:app/media/icons/settings.svg')" width="30" height="30" data-uk-svg alt="assets" /> @lang('Settings')</a></li>
                                             @end
 
                                         </ul>
