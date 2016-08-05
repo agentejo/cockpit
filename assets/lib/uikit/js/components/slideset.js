d1fe3f1833f26d7f4349b66af8ef71e07dbebc5f
Artur Heinze <artur@yootheme.com>
2016-08-05 18:40:09 +0200

    UIkit updated

--- assets/lib/uikit/js/components/slideset.js
+++ assets/lib/uikit/js/components/slideset.js
@@ -61,7 +61,7 @@
             this.controls  = this.options.controls ? UI.$(this.options.controls) : this.element;
 
             UI.$win.on("resize load", UI.Utils.debounce(function() {
-                $this.updateSets();
+                $this.update();
             }, 100));
 
             $this.list.addClass('uk-grid-width-1-'+$this.options.default);
@@ -116,7 +116,7 @@
 
                 $this._hide().then(function(){
 
-                    $this.updateSets(true, true);
+                    $this.update(true, true);
                 });
             });
 
@@ -125,7 +125,7 @@
             });
 
             this.updateFilter(this.options.filter);
-            this.updateSets();
+            this.update();
 
             this.element.on({
                 mouseenter: function() { if ($this.options.pauseOnHover) $this.hovering = true;  },
@@ -136,9 +136,15 @@
             if (this.options.autoplay) {
                 this.start();
             }
+
+            UI.domObserve(this.list, function(e) {
+                if ($this.list.children(':visible:not(.uk-active)').length) {
+                    $this.update(false,true);
+                }
+            });
         },
 
-        updateSets: function(animate, force) {
+        update: function(animate, force) {
 
             var visible = this.visible, i;
 
--- modules/core/Cockpit/views/webhooks/webhook.php
+++ modules/core/Cockpit/views/webhooks/webhook.php
@@ -25,6 +25,56 @@
                     <input class="uk-width-1-1 uk-form-large" type="url" bind="webhook.url" required>
                 </div>
 
+                <div class="uk-form-row uk-panel uk-panel-box uk-panel-box-secondary" show="{advanced}">
+                    
+                    <span class="uk-badge">Advanced</span>
+                
+                    <div class="uk-margin">
+                        <label class="uk-text-small uk-text-uppercase">HTTP Basic Auth</label>
+
+                        <div class="uk-margin uk-grid uk-grid-small uk-grid-width-1-1">
+                            <div>
+                                <div class="uk-form-icon uk-width-1-1 uk-display-block">
+                                    <i class="uk-icon-user"></i>
+                                    <input class="uk-width-1-1" type="text" bind="webhook.auth.user" placeholder="@lang('User')">
+                                </div>
+                            </div>
+                            <div class="uk-grid-margin">
+                                <div class="uk-form-icon uk-width-1-1 uk-display-block">
+                                    <i class="uk-icon-key"></i>
+                                    <input class="uk-width-1-1" type="password" bind="webhook.auth.pass" placeholder="@lang('Password')">
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <div class="uk-margin">
+                        <label class="uk-text-small uk-text-uppercase">@lang('Custom Headers')</label>
+
+                        <div class="uk-margin uk-panel uk-form">
+                            
+                            <div class="uk-grid uk-grid-small" each="{h,idx in webhook.headers}">
+                                <div class="uk-flex-item-1"><input class="uk-width-1-1" type="text" placeholder="Key" bind="webhook.headers[{idx}].k"></div>
+                                <div>:</div>
+                                <div class="uk-flex-item-1"><input class="uk-width-1-1" type="text" placeholder="Value" bind="webhook.headers[{idx}].v"></div>
+                                <div><a onclick="{ parent.removeHeader}"><i class="uk-text-danger uk-icon-trash"></i></a></div>
+                            </div>
+
+                            <div class="uk-margin uk-text-center {!webhook.headers.length && 'uk-placeholder'}">
+                                <a class="uk-button uk-button-success" onclick="{ addHeader }"><i class="uk-icon-plus-circle"></i><span class="uk-margin-small-left" show="{!webhook.headers.length}">@lang('Add custom header')</span></a>
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div class="uk-form-row">
+                    <a class="uk-button uk-button-small" onclick="{ toggleAdvance.bind(this, true) }" show="{!advanced}"><i class="uk-icon-cog"></i> @lang('Show Advanced Options')</a>
+                    <a class="uk-button uk-button-small" onclick="{ toggleAdvance.bind(this, false) }" show="{advanced}"><i class="uk-icon-cog"></i> @lang('Hide')</a>
+                </div>
+
                 <div class="uk-form-row">
                     <label class="uk-text-small">@lang('Events')</label>
 
@@ -80,7 +130,8 @@
 
         this.mixin(RiotBindMixin);
 
-        this.webhook = {{ json_encode($webhook) }};
+        this.webhook  = {{ json_encode($webhook) }};
+        this.advanced = false;
 
         this.on('mount', function(){
 
@@ -129,6 +180,18 @@
                     App.ui.notify("Saving failed.", "danger");
                 }
             });
+        }
+
+        addHeader() {
+            this.webhook.headers.push({k:'',v:''});
+        }
+
+        removeHeader(evt) {
+            this.webhook.headers.splice(evt.item.idx, 1);
+        }
+
+        toggleAdvance(status) {
+            this.advanced = status;
         }
 
 
