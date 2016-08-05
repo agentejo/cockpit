e0308c816937fa3a9a98354d194541e47ebfb22e
Artur <artur@agentejo.com>
2016-08-05 23:54:58 +0200

    send custom headers with web hooks

--- modules/core/Cockpit/webhooks.php
+++ modules/core/Cockpit/webhooks.php
@@ -15,21 +15,30 @@ foreach ($webhooks as &$webhook) {
 
             $app->on($evt, function() use($evt, $webhook) {
 
-                $ch   = curl_init($webhook['url']);
-                $data = json_encode([
-                    'event' => $evt,
-                    'args' => func_get_args()
-                ]);
-
-                $header = [
+                $ch      = curl_init($webhook['url']);
+                $data    = json_encode(['event' => $evt, 'args' => func_get_args()]);
+                $headers = [
                     'Content-Type: application/json',
                     'Content-Length: '.strlen($data)
                 ];
 
+                // add custom headers
+                if (is_array($webhook['headers']) && count($webhook['headers'])) {
+
+                    foreach ($webhook['headers'] as $h) {
+                        
+                        if (!isset($h['k'], $h['v']) || !$h['k'] || !$h['v']) {
+                            continue;
+                        }
+
+                        $headers[] = implode(': ', [$h['k'], $h['v']]);
+                    }
+                }
+
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
                 curl_exec($ch);
 
