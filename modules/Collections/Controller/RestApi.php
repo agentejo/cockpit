b13b9a994d117f0012f45e68b969ac894ae1bdff
Artur <artur@agentejo.com>
2016-10-06 22:03:32 +0200

    Fixes #473

--- modules/Collections/Controller/RestApi.php
+++ modules/Collections/Controller/RestApi.php
@@ -12,12 +12,18 @@ class RestApi extends \LimeExtra\Controller {
         $options = [];
 
         if ($filter   = $this->param("filter", null))   $options["filter"] = $filter;
-        if ($limit    = $this->param("limit", null))    $options["limit"] = $limit;
+        if ($limit    = $this->param("limit", null))    $options["limit"] = intval($limit);
         if ($sort     = $this->param("sort", null))     $options["sort"] = $sort;
-        if ($skip     = $this->param("skip", null))     $options["skip"] = $skip;
+        if ($skip     = $this->param("skip", null))     $options["skip"] = intval($skip);
         if ($populate = $this->param("populate", null)) $options["populate"] = $populate;
 
-        $entries    = $this->app->module('collections')->find($collection, $options);
+        if (isset($options["sort"])) {
+            foreach ($sort as $key => &$value) {
+                $options["sort"][$key]= intval($value);
+            }
+        }
+
+        $entries = $this->app->module('collections')->find($collection, $options);
 
         // return only entries array - due to legacy
         if ((boolean) $this->param("simple", false)) {
