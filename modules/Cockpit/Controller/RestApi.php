bac26144246eb37efc5e111dc6ca4a40befb56b4
Artur <artur@agentejo.com>
2017-04-05 16:12:10 +0200

    add /api/cockpit/authUser endpoint to authenticate/validate users

--- modules/Cockpit/Controller/RestApi.php
+++ modules/Cockpit/Controller/RestApi.php
@@ -3,4 +3,23 @@ namespace Cockpit\Controller;
 
 class RestApi extends \LimeExtra\Controller {
 
+    public function authUser() {
+
+        $response = ['error' => 'Authentication failed'];
+        $data     = [ 'user' => $this->param('user'), 'password' => $this->param('password') ];
+
+        if (!$data['user'] || !$data['password']) {
+            return $response;
+        }
+
+        $user = $this->module('cockpit')->authenticate($data);
+
+        if ($user) {
+            $response = $user;
+        }
+
+        return $response;
+    }
+
+    
 }
