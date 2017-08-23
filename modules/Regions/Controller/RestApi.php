8b06303f07722f4403de16dc97de1c4b61928348
Artur <artur@agentejo.com>
2017-08-23 14:44:52 +0200

    fix /api/regions/data

--- modules/Regions/Controller/RestApi.php
+++ modules/Regions/Controller/RestApi.php
@@ -27,7 +27,13 @@ class RestApi extends \LimeExtra\Controller {
             return false;
         }
 
-        $region = $this->region($name);
+        $region = $this->module('regions')->region($name);
+
+        if ($this->module('cockpit')->getUser()) {
+            if (!$this->module('regions')->hasaccess($name, 'form')) {
+                return $this->stop(401);
+            }
+        }
 
         if (!$region) {
             return false;
