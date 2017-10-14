efb8d1e4c238a94bb4ada7650f6a1a354184ca91
Simon <net@backtick.ch>
2017-10-14 22:18:50 +0200

    Fix hard-coded string
    Fix an instance of hard-coded string label in entry view

--- modules/Collections/views/entry.php
+++ modules/Collections/views/entry.php
@@ -34,7 +34,7 @@
 
     <h3 class="uk-flex uk-flex-middle uk-text-bold">
         <img class="uk-margin-small-right" src="@url($collection['icon'] ? 'assets:app/media/icons/'.$collection['icon']:'collections:icon.svg')" width="25" alt="icon">
-        { entry._id ? 'Edit':'Add' } @lang('Entry')
+        @lang(entry._id ? 'Edit Entry':'Add Entry')
     </h3>
 
     <div class="uk-grid">
