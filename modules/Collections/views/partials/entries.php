c58ceb8716beb871c102b2b8a002c8475c519e7d
Artur <artur@agentejo.com>
2017-11-10 16:33:33 +0100

    add list & grid option to collection entries view

--- modules/Collections/views/partials/entries.php
+++ modules/Collections/views/partials/entries.php
@@ -3,7 +3,64 @@
     <div>@lang('No entries found')</div>
 </div>
 
-<table class="uk-table uk-table-border uk-table-striped" if="{ entries.length && !loading }">
+
+<div class="uk-grid uk-grid-match uk-grid-width-medium-1-4 uk-flex-center" if="{ entries.length && !loading && listmode=='grid' }">
+
+    <div class="uk-grid-margin" each="{entry,idx in entries}">
+
+        <div class="uk-panel uk-panel-box uk-panel-card">
+
+            <div class="uk-panel-teaser uk-position-relative">
+                <canvas width="400" height="250"></canvas>
+                <div class="uk-position-cover uk-flex uk-flex-center uk-flex-middle">
+                    <div class="uk-svg-adjust uk-text-primary" style="color:{{ @$collection['color'] }} !important;">
+                        <img src="@url($collection['icon'] ? 'assets:app/media/icons/'.$collection['icon']:'collections:icon.svg')" width="80" alt="icon" data-uk-svg>
+                    </div>
+                </div>
+                <a class="uk-position-cover" href="@route('/collections/entry/'.$collection['name'])/{ entry._id }"></a>
+            </div>
+            <div class="uk-flex uk-flex-middle uk-margin-small-top">
+
+                <div class="uk-flex-item-1 uk-margin-small-right">
+                    <span class="uk-badge uk-badge-outline uk-text-muted uk-margin-small-right">{ App.Utils.dateformat( new Date( 1000 * entry._created )) }</span>
+                    <span class="uk-badge uk-badge-outline uk-text-primary">{ App.Utils.dateformat( new Date( 1000 * entry._modified )) }</span>
+                </div>
+                <span data-uk-dropdown="mode:'click', pos:'bottom-right'">
+
+                    <a class="uk-icon-bars"></a>
+
+                    <div class="uk-dropdown uk-dropdown-flip">
+                        <ul class="uk-nav uk-nav-dropdown">
+                            <li class="uk-nav-header">@lang('Actions')</li>
+                            <li><a href="@route('/collections/entry/'.$collection['name'])/{ entry._id }">@lang('Edit')</a></li>
+
+                            @if($app->module('collections')->hasaccess($collection['name'], 'entries_delete'))
+                            <li><a class="uk-dropdown-close" onclick="{ parent.remove }">@lang('Delete')</a></li>
+                            @endif
+
+                            @if($app->module('collections')->hasaccess($collection['name'], 'entries_create'))
+                            <li class="uk-nav-divider"></li>
+                            <li><a class="uk-dropdown-close" onclick="{ parent.duplicateEntry }">@lang('Duplicate')</a></li>
+                            @endif
+                        </ul>
+                    </div>
+                </span>
+            </div>
+            <div class="uk-margin-top uk-scrollable-box">
+                <div class="uk-margin-small-bottom" each="{field,idy in parent.fields}" if="{ field.name != '_modified' && field.name != '_created' && hasFieldAccess(field.name) }">
+                    <span class="uk-text-small uk-text-uppercase uk-text-muted">{ field.label || field.name }</span>
+                    <a class="uk-link-muted uk-text-small uk-display-block uk-text-truncate" href="@route('/collections/entry/'.$collection['name'])/{ parent.entry._id }">
+                        <raw content="{ App.Utils.renderValue(field.type, parent.entry[field.name]) }" if="{parent.entry[field.name] !== undefined}"></raw>
+                        <span class="uk-icon-eye-slash uk-text-muted" if="{parent.entry[field.name] === undefined}"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<table class="uk-table uk-table-border uk-table-striped uk-margin-large-top" if="{ entries.length && !loading && listmode=='list' }">
     <thead>
         <tr>
             @if($app->module('collections')->hasaccess($collection['name'], 'entries_delete'))
@@ -31,7 +88,7 @@
                     <span class="uk-icon-eye-slash uk-text-muted" if="{parent.entry[field.name] === undefined}"></span>
                 </a>
             </td>
-            <td><span class="uk-badge uk-badge-outline uk-text-muted">{ App.Utils.dateformat( new Date( 1000 * entry._created )) }<span></td>
+            <td><span class="uk-badge uk-badge-outline uk-text-muted">{ App.Utils.dateformat( new Date( 1000 * entry._created )) }</span>span></td>
             <td><span class="uk-badge uk-badge-outline uk-text-primary">{ App.Utils.dateformat( new Date( 1000 * entry._modified )) }</span></td>
             <td>
                 <span data-uk-dropdown="mode:'click'">
@@ -59,11 +116,8 @@
     </tbody>
 </table>
 
-<div class="uk-alert" if="{ loading }">
-    <i class="uk-icon-spinner uk-icon-spin"></i> @lang('Loading...').
-</div>
 
-<div class="uk margin uk-flex uk-flex-middle" if="{ !loading && pages > 1 }">
+<div class="uk-margin uk-flex uk-flex-middle" if="{ !loading && pages > 1 }">
 
     <ul class="uk-breadcrumb uk-margin-remove">
         <li class="uk-active"><span>{ page }</span></li>
