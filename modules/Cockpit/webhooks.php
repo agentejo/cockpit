88c40684a1632736c2f7dedcf5632b17491a6bf4
Artur <artur@agentejo.com>
2017-12-23 00:57:55 +0100

    try to improve webhook performance

--- modules/Cockpit/webhooks.php
+++ modules/Cockpit/webhooks.php
@@ -15,14 +15,14 @@ foreach ($webhooks as &$webhook) {
 
             $app->on($evt, function() use($evt, $webhook) {
 
-                $ch      = curl_init(trim($webhook['url']));
-                $data    = json_encode([
+                $url = trim($webhook['url']);
+                $data = json_encode([
                     'event' => $evt,
                     'hook'  => $webhook['name'],
-                    'backend' => COCKPIT_ADMIN, 
+                    'backend' => COCKPIT_ADMIN,
                     'args' => func_get_args()
                 ]);
-                
+
                 $headers = [
                     'Content-Type: application/json',
                     'Content-Length: '.strlen($data)
@@ -32,13 +32,15 @@ foreach ($webhooks as &$webhook) {
                 if (isset($webhook['headers']) && is_array($webhook['headers']) && count($webhook['headers'])) {
 
                     foreach ($webhook['headers'] as &$h) {
-                        
+
                         if (!isset($h['k'], $h['v']) || !$h['k'] || !$h['v']) {
                             continue;
                         }
                         $headers[] = implode(': ', [$h['k'], $h['v']]);
                     }
                 }
+
+                $ch = curl_init($url);
 
                 // add basic http auth
                 if (isset($webhook['auth']) && $webhook['auth']['user'] && $webhook['auth']['pass']) {
@@ -47,15 +49,13 @@ foreach ($webhooks as &$webhook) {
 
                 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
                 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 1);
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-                if (curl_exec($ch) === false) {
-                    trigger_error(curl_error($ch));
-                }
-
+                curl_exec($ch);
                 curl_close($ch);
 
             }, -1000);
