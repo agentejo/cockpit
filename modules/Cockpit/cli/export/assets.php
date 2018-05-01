d43eafba19ef18dd2dcbce3889b9f9d0b05b83e5
Artur <artur@agentejo.com>
2018-05-01 23:42:20 +0200

    export assets files

--- modules/Cockpit/cli/export/assets.php
+++ modules/Cockpit/cli/export/assets.php
@@ -8,11 +8,24 @@ if (!$target) {
     return;
 }
 
+$fs = $app->helper('fs');
 $assets = $app->storage->find('cockpit/assets')->toArray();
 
 if (count($assets)) {
 
     CLI::writeln("Exporting cockpit/assets (".count($assets).")");
 
-    $app->helper('fs')->write("{$target}/cockpit/assets.json", json_encode($assets, JSON_PRETTY_PRINT));
+    $fs->write("{$target}/cockpit/assets.json", json_encode($assets, JSON_PRETTY_PRINT));
+
+    // move assets files
+    foreach ($assets as &$asset) {
+
+        $path = trim($asset['path'], '/');
+        $_target = "{$target}/cockpit/assets/{$path}";
+
+        if ($_path = $app->path("#uploads:{$path}")) {
+            $fs->mkdir(dirname($_target));
+            $fs->copy($_path, $_target);
+        }
+    }
 }
