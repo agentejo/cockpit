a75d5d2cf389e3d7e5aea1d512310179431e6206
Paulo Gomes <pauloamgomes@gmail.com>
2018-08-22 23:36:56 +0100

    updated load of config values and defaults

--- index.php
+++ index.php
@@ -21,12 +21,12 @@ if (COCKPIT_ADMIN && !defined('COCKPIT_ADMIN_ROUTE')) {
 
 if (COCKPIT_API_REQUEST) {
 
-    header("Access-Control-Allow-Origin: ".$cockpit['config']['cors']['allowedOrigins']);
-    header("Access-Control-Allow-Credentials: ".$cockpit['config']['cors']['allowCredentials']);
-    header("Access-Control-Max-Age: ".$cockpit['config']['cors']['maxAge']);
-    header("Access-Control-Allow-Headers: ".$cockpit['config']['cors']['allowedHeaders']);
-    header("Access-Control-Allow-Methods: ".$cockpit['config']['cors']['allowedMethods']);
-    header("Access-Control-Expose-Headers: ".$cockpit['config']['cors']['exposedHeaders']);
+    header("Access-Control-Allow-Origin: ".$cockpit->retrieve('config/cors/allowedOrigins', '*'));
+    header("Access-Control-Allow-Credentials: ".$cockpit->retrieve('config/cors/allowCredentials', 'true'));
+    header("Access-Control-Max-Age: ".$cockpit->retrieve('config/cors/maxAge', '1000'));
+    header("Access-Control-Allow-Headers: ".$cockpit->retrieve('config/cors/allowedHeaders', 'X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding'));
+    header("Access-Control-Allow-Methods: ".$cockpit->retrieve('config/cors/allowedMethods', 'PUT, POST, GET, OPTIONS, DELETE'));
+    header("Access-Control-Expose-Headers: ".$cockpit->retrieve('config/cors/exposedHeaders', 'true'));
 
     if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
         exit(0);
