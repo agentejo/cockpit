b832984ed5a882c1f9c1e0aa45739d6120fd96b5
Artur <artur@agentejo.com>
2018-08-31 21:31:08 +0200

    add create-lang cli command to create dummy language files

---
+++ modules/Cockpit/cli/create-lang.php
@@ -0,0 +1,55 @@
+<?php
+
+if (!COCKPIT_CLI) return;
+
+$lang     = $app->param('lang', null);
+$language = $app->param('language', $lang);
+$author   = $app->param('author', 'Cockpit CLI');
+
+if (!$lang) {
+    return CLI::writeln("--lang parameter is missing", false);
+}
+
+// settings
+$extensions = ['php', 'md', 'html', 'js', 'tag'];
+$strings    = [];
+
+
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(COCKPIT_DIR), RecursiveIteratorIterator::SELF_FIRST);
+
+foreach ($iterator as $file) {
+
+    if (in_array($file->getExtension(), $extensions)) {
+
+        $contents = file_get_contents($file->getRealPath());
+
+        preg_match_all('/(?:\@lang|App\.i18n\.get|App\.ui\.notify)\((["\'])((?:[^\1]|\\.)*?)\1(,\s*(["\'])((?:[^\4]|\\.)*?)\4)?\)/', $contents, $matches);
+
+        if (!isset($matches[2])) continue;
+
+        foreach ($matches[2] as &$string) {
+            $strings[$string] = $string;
+        }
+
+    }
+}
+
+if (count($strings)) {
+
+    $strings['@meta'] = [
+        'language' => $language,
+        'author'   => $author,
+        'date' => [
+            'shortdays'   => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            'longdays'    => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+            'shortmonths' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            'longmonths'  => ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        ]
+    ];
+
+    ksort($strings);
+
+    $app->helper('fs')->write("#config:cockpit/i18n/{$lang}.php", '<?php return '.var_export($strings, true));
+}
+
+CLI::writeln("Done! Language file created: config/cockpit/i18n/{$lang}.php", true);
