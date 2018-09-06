2c9afe408a1bc6786487a523901960284f3d6d8e
Artur <artur@agentejo.com>
2018-09-06 18:20:07 +0200

    Allow Bearer Token for api auth #852

--- lib/Lime/Helper/Utils.php
+++ lib/Lime/Helper/Utils.php
@@ -327,4 +327,35 @@ class Utils extends \Lime\Helper {
 
         return $depth == 0 ? $result->getArrayCopy() : $result;
     }
+
+	/**
+	 * get access token from header
+	 * */
+	public function getBearerToken() {
+
+	    $headers = null;
+	    $token   = null;
+
+	    if (isset($_SERVER['Authorization'])) {
+	        $headers = trim($_SERVER['Authorization']);
+	    } elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+	        $headers = trim($_SERVER['HTTP_AUTHORIZATION']);
+	    } elseif (function_exists('apache_request_headers')) {
+	        $requestHeaders = apache_request_headers();
+	        // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+	        $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+	        if (isset($requestHeaders['Authorization'])) {
+	            $headers = trim($requestHeaders['Authorization']);
+	        }
+	    }
+
+	    // HEADER: Get the access token from the header
+	    if ($headers) {
+	        if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+	            $token = $matches[1];
+	        }
+	    }
+
+	    return $token;
+	}
 }
