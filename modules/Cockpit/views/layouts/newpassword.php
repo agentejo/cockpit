764f0066fa695656e2fc84497119a51db2490aba
Artur <artur@agentejo.com>
2018-09-14 00:39:12 +0200

    some views refactoring

--- modules/Cockpit/views/layouts/newpassword.php
+++ modules/Cockpit/views/layouts/newpassword.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ $app('i18n')->locale }}" class="uk-height-1-1 uk-bg-dark" data-base="@base('/')" data-route="@route('/')" data-locale="{{ $app('i18n')->locale }}">
+<html lang="{{ $app('i18n')->locale }}" class="uk-height-1-1" data-base="@base('/')" data-route="@route('/')" data-locale="{{ $app('i18n')->locale }}">
 <head>
     <meta charset="UTF-8">
     <title>@lang('Password Reset!')</title>
@@ -12,12 +12,16 @@
     <style>
 
         .container {
-            width: 360px;
+            width: 420px;
             max-width: 90%;
         }
 
         .uk-panel-box-header {
             border-bottom: none;
+        }
+
+        .newpassword-dialog {
+            box-shadow: 0 30px 75px 0 rgba(10, 25, 41, 0.2);
         }
 
         svg path,
@@ -31,21 +35,21 @@
 </head>
 <body class="newpassword-page uk-height-viewport uk-flex uk-flex-middle uk-flex-center">
 
-    <div class="uk-position-relative container uk-animation-slide-bottom" riot-view>
+    <div class="uk-position-relative container uk-animation-scale" riot-view>
 
         <form class="uk-form" method="post" action="@route('/auth/check')" onsubmit="{ submit }">
 
-            <div id="newpassword-dialog" class="uk-panel-box uk-panel-space uk-panel-card uk-nbfc" show="{!$user}">
+            <div id="newpassword-dialog" class="newpassword-dialog uk-panel-box uk-panel-space uk-panel-card uk-nbfc" show="{!$user}">
 
                 <div name="header" class="uk-panel-box-header uk-text-bold uk-text-center">
 
                     <p>
-                        <img src="@url('assets:app/media/icons/password-reset.svg')" width="80" alt="User" data-uk-svg />
+                        <img src="@url('assets:app/media/icons/password-reset.svg')" width="80" height="80" alt="" />
                     </p>
 
                     <h2 class="uk-text-bold uk-text-truncate"><span>{{ $app['app.name'] }}</span></h2>
 
-                    <p class="uk-text-primary">{ user.name || user.user}</p>
+                    <p class="uk-text-bold">{ user.name || user.user}</p>
 
                     <div class="uk-animation-shake uk-margin-top" if="{ error }">
                         <strong>{ error }</strong>
