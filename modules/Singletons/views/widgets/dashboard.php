108cd6f961b2cc8c67e0a0d00e4182e15a6042fc
Artur <artur@agentejo.com>
2018-11-12 00:59:57 +0100

    minor ui improvements

--- modules/Singletons/views/widgets/dashboard.php
+++ modules/Singletons/views/widgets/dashboard.php
@@ -22,7 +22,7 @@
 
                 <ul class="uk-list uk-list-space uk-margin-top">
                     @foreach(array_slice($singletons, 0, count($singletons) > 5 ? 5: count($singletons)) as $singleton)
-                    <li>
+                    <li class="uk-text-truncate">
                         <a href="@route('/singletons/form/'.$singleton['name'])">
 
                             <img class="uk-margin-small-right uk-svg-adjust" src="@url(isset($singleton['icon']) && $singleton['icon'] ? 'assets:app/media/icons/'.$singleton['icon']:'singletons:icon.svg')" width="18px" alt="icon" data-uk-svg>
