a5c788e7c795c0f01d328a752d61eb789eb92b4e
Artur Heinze <artur@agentejo.com>
2018-12-04 00:12:22 +0100

    fix styling for error pages

--- modules/Cockpit/views/errors/404.php
+++ modules/Cockpit/views/errors/404.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Page not found</title>
     <link rel="icon" href="@base('/favicon.ico')" type="image/x-icon">
-    {{ $app->assets($app['app.assets.base'], $app['cockpit/version']) }}
+    {{ $app->assets(['assets:app/css/style.css'], $app['cockpit/version']) }}
 </head>
 <body class="uk-height-viewport uk-flex uk-flex-middle">
 
