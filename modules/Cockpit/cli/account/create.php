069115c98ed8359421b5fde6d94eca04422ea668
Artur Heinze <artur@agentejo.com>
2019-01-31 14:41:09 +0100

    Added two new cli commands:
    - ./cp account/generate-password --passwd <password>
    - ./cp account/create --user <username> --email <email> --passwd <encrypted_password>

---
+++ modules/Cockpit/cli/account/create.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * This file is part of the Cockpit project.
+ *
+ * (c) Artur Heinze - Agentejo, http://agentejo.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!COCKPIT_CLI) return;
+
+$user = $app->param('user', null);
+
+    if (!$user) {
+        return CLI::writeln('--user parameter is missing', false);
+    }
+
+$email = $app->param('email', null);
+
+    if (!$email) {
+        return CLI::writeln('--email parameter is missing', false);
+    }
+
+    if (!$app->helper('utils')->isEmail($email)) {
+        return CLI::writeln('Valid email required', false);
+    }
+
+$password = $app->param('passwd', null);
+
+    if (!$password) {
+        return CLI::writeln('--passwd (hashed password) parameter is missing', false);
+    }
+
+$created = time();
+
+$account = [
+    'user'     => $user,
+    'name'     => $app->param('name', $user),
+    'email'    => $email,
+    'password' => $password,
+    'active'   => true,
+    'group'    => $app->param('group', 'admin'),
+    'i18n'     => $app->param('i18n', 'en'),
+    '_created' => $created,
+    '_modified'=> $created,
+];
+
+// unique check
+// --
+
+$exist = $app->storage->findOne('cockpit/accounts', ['user'  => $user]);
+
+    if ($exist) {
+        return CLI::writeln('Username is already used!', false);
+    }
+
+$exist = $app->storage->findOne('cockpit/accounts', ['email'  => $email]);
+
+    if ($exist) {
+        return CLI::writeln('Email is already used!', false);
+    }
+
+// --
+
+$app->storage->insert('cockpit/accounts', $account);
+
+CLI::writeln('Account created', true);
+
+// ./cp account/create --user cliuser --email cliuser@huhu.de --passwd $2y$10$iJArAtk8DJJe3YEMDL.Boe7YxWnJiuQNxjmvIWMZ2viZME.tZ41ma
---
+++ modules/Cockpit/cli/account/generate-password.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the Cockpit project.
+ *
+ * (c) Artur Heinze - Agentejo, http://agentejo.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!COCKPIT_CLI) return;
+
+$password = $app->param('passwd', null);
+
+if (!$password) {
+    return CLI::writeln("--passwd parameter is missing", false);
+}
+
+$password = $app->hash($password);
+
+CLI::writeln($password, true);
