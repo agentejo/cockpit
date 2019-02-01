b3fa8f4db365eb513e13eececcf40f2d7473fffa
Artur Heinze <artur@agentejo.com>
2019-02-01 13:37:19 +0100

    Fix #987

--- modules/Collections/Controller/RestApi.php
+++ modules/Collections/Controller/RestApi.php
@@ -46,11 +46,10 @@ class RestApi extends \LimeExtra\Controller {
         if ($populate = $this->param('populate', null)) $options['populate'] = $populate;
 
         // cast string values if get request
-        if ($filter && isset($_GET['filter'])) $options['filter'] = $this->_fixStringBooleanValues($filter);
-        if ($fields && isset($_GET['fields'])) $options['fields'] = $this->_fixStringBooleanValues($fields);
+        if ($filter && isset($_GET['filter'])) $options['filter'] = $this->app->helper('utils')->fixStringBooleanValues($filter);
+        if ($fields && isset($_GET['fields'])) $options['fields'] = $this->app->helper('utils')->fixStringNumericValues($fields);
 
         // fields filter
-
         if ($fieldsFilter = $this->param('fieldsFilter', [])) $options['fieldsFilter'] = $fieldsFilter;
         if ($lang = $this->param('lang', false)) $fieldsFilter['lang'] = $lang;
         if ($ignoreDefaultFallback = $this->param('ignoreDefaultFallback', false)) $fieldsFilter['ignoreDefaultFallback'] = in_array($ignoreDefaultFallback, ['1', '0']) ? boolval($ignoreDefaultFallback) : $ignoreDefaultFallback;
@@ -250,30 +249,5 @@ class RestApi extends \LimeExtra\Controller {
         }
 
         return $extended ? $collections : array_keys($collections);
-    }
-
-    protected function _fixStringBooleanValues(&$array) {
-
-        if (!is_array($array)) {
-            return $array;
-        }
-
-        foreach ($array as $k => $v) {
-
-            if (is_array($array[$k])) {
-                $array[$k] = $this->_fixStringBooleanValues($array[$k]);
-            }
-
-            if (is_string($v)) {
-
-                if ($v === 'true' || $v === 'false') {
-                    $v = filter_var($v, FILTER_VALIDATE_BOOLEAN);
-                }
-            }
-
-            $array[$k] = $v;
-        }
-
-        return $array;
     }
 }
