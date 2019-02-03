c79d17719ab95f9e9c783c81cff44094fe0d61a1
Artur Heinze <artur@agentejo.com>
2019-02-04 00:43:30 +0100

    update branding

--- lib/Pagination.php
+++ lib/Pagination.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Cockpit project.
  *
- * (c) Artur Heinze - Agentejo, http://agentejo.com
+ * (c) Artur Heinze - 🅰🅶🅴🅽🆃🅴🅹🅾, http://agentejo.com
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
