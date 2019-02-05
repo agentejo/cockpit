e4c58f02b3ed4a77c4e8d03077e41b8710d3bf81
Artur Heinze <artur@agentejo.com>
2019-02-05 22:13:16 +0100

    additional styling

--- modules/Singletons/views/index.php
+++ modules/Singletons/views/index.php
@@ -56,7 +56,7 @@
 
             <div each="{ singleton,idx in singletons }" show="{ ingroup(singleton.meta) && infilter(singleton.meta) }">
 
-                <div class="uk-panel uk-panel-box uk-panel-card">
+                <div class="uk-panel uk-panel-box uk-panel-card uk-panel-card-hover">
 
                     <div class="uk-panel-teaser uk-position-relative">
                         <canvas width="600" height="350"></canvas>
