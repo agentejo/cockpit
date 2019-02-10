e873a6ee88a21456d0a40cfb048a20acf7acbe20
Artur Heinze <artur@agentejo.com>
2019-02-10 02:40:59 +0100

    replace favicon.ico with favicon.png (80kb to 4kb)

--- modules/Cockpit/views/layouts/app.php
+++ modules/Cockpit/views/layouts/app.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ implode(' &raquo; ', $_title).(count($_title) ? ' - ':'').$app['app.name'] }}</title>
-    <link rel="icon" href="@base('/favicon.ico')" type="image/x-icon">
+    <link rel="icon" href="@base('/favicon.png')" type="image/png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
     <script>
--- modules/Cockpit/views/layouts/newpassword.php
+++ modules/Cockpit/views/layouts/newpassword.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@lang('Password Reset!')</title>
-    <link rel="icon" href="@base('/favicon.ico')" type="image/x-icon">
+    <link rel="icon" href="@base('/favicon.png')" type="image/png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
     {{ $app->assets($app['app.assets.base'], $app['cockpit/version']) }}
