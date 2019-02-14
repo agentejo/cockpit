781d66d1ae6650e9041263527d3b6e7e29b677f5
Artur Heinze <artur@agentejo.com>
2019-02-14 15:28:08 +0100

    cleanup

--- modules/Collections/admin.php
+++ modules/Collections/admin.php
@@ -78,4 +78,28 @@ $app->on('admin.init', function() {
         ];
 
     }, 100);
+
+    // register events for autocomplete
+    $this->on('cockpit.webhook.events', function($triggers) {
+
+        foreach([
+            'collections.createcollection',
+            'collections.find.after',
+            'collections.find.after.{$name}',
+            'collections.find.before',
+            'collections.find.before.{$name}',
+            'collections.remove.after',
+            'collections.remove.after.{$name}',
+            'collections.remove.before',
+            'collections.remove.before.{$name}',
+            'collections.removecollection',
+            'collections.removecollection.{$name}',
+            'collections.save.after',
+            'collections.save.after.{$name}',
+            'collections.save.before',
+            'collections.save.before.{$name}',
+            'collections.updatecollection',
+            'collections.updatecollection.{$name}'
+        ] as &$evt) { $triggers[] = $evt; }
+    });
 });
