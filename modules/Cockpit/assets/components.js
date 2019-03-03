5e5deca257c76773600cdc594f8c6c2c4735f54a
blistok <slobodan.syd@gmail.com>
2019-03-03 00:40:14 -0500

    Fix Location component z-index

--- modules/Cockpit/assets/components.js
+++ modules/Cockpit/assets/components.js
@@ -3038,7 +3038,7 @@ riot.tag2('field-layout-grid', '<div class="uk-text-center uk-placeholder" if="{
 
 });
 
-riot.tag2('field-location', '<div class="uk-alert" if="{!apiready}"> Loading maps api... </div> <div show="{apiready}"> <div class="uk-form uk-position-relative uk-margin-small-bottom uk-width-1-1" style="z-index:1001"> <input ref="autocomplete" class="uk-width-1-1" placeholder="{latlng.address || [latlng.lat, latlng.lng].join(\', \')}"> </div> <div ref="map" style="min-height:300px;"> Loading map... </div> </div>', '', '', function(opts) {
+riot.tag2('field-location', '<div class="uk-alert" if="{!apiready}"> Loading maps api... </div> <div show="{apiready}"> <div class="uk-form uk-position-relative uk-margin-small-bottom uk-width-1-1" style="z-index:1001"> <input ref="autocomplete" class="uk-width-1-1" placeholder="{latlng.address || [latlng.lat, latlng.lng].join(\', \')}"> </div> <div ref="map" style="min-height:300px; z-index:0;"> Loading map... </div> </div>', '', '', function(opts) {
 
         var map, marker;
 
