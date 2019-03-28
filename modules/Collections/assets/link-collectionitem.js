14f94db24154170d7392c8306a6d15ba0e4a9ca3
Gryphon Myers <gryphon.myers@2k.com>
2019-03-28 14:54:02 -0700

    fix for collection link display

--- modules/Collections/assets/link-collectionitem.js
+++ modules/Collections/assets/link-collectionitem.js
@@ -8,8 +8,11 @@
             return '<i class="uk-icon-eye-slash uk-text-muted"></i>';
         }
 
-        if (Array.isArray(v) && v.length > 1) {
-            return `<span class="uk-badge ${!v.length && 'uk-badge-outline uk-text-muted'}">${v.length}</span>`;
+        if (Array.isArray(v)) {
+            if (v.length > 1) {
+                return `<span class="uk-badge ${!v.length && 'uk-badge-outline uk-text-muted'}">${v.length}</span>`;
+            }
+            v = v[0];
         }
 
         if (!linkCache[v._id]) {
