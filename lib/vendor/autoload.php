468a67dd2eabf6aab49a23cc96e7987f34512104
Artur Heinze <artur@agentejo.com>
2019-04-18 21:27:38 +0200

    update libs

--- lib/vendor/autoload.php
+++ lib/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit669399906f705702d64bc0966a31f62b::getLoader();
+return ComposerAutoloaderInit3043aff3db8446d21c22f36f77adce25::getLoader();
