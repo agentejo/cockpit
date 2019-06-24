98ea98611aa7d16bcf14af8711e4b4bf8bd3826f
Paulo Gomes <pauloamgomes@gmail.com>
2019-06-24 22:07:06 +0100

    added event for manipulate admin find options

--- modules/Collections/Controller/Admin.php
+++ modules/Collections/Controller/Admin.php
@@ -281,7 +281,7 @@ class Admin extends \Cockpit\AuthController {
 
             $_entry = $this->module('collections')->findOne($collection['name'], ['_id' => $entry['_id']]);
             $revision = !(json_encode($_entry) == json_encode($entry));
-            
+
         } else {
 
             $entry['_by'] = $entry['_mby'];
@@ -399,6 +399,7 @@ class Admin extends \Cockpit\AuthController {
             $options['filter'] = $this->_filter($options['filter'], $collection);
         }
 
+        $this->app->trigger("collections.admin.find.before.{$collection['name']}", [&$options]);
         $entries = $this->app->module('collections')->find($collection['name'], $options);
         $count   = $this->app->module('collections')->count($collection['name'], isset($options['filter']) ? $options['filter'] : []);
         $pages   = isset($options['limit']) ? ceil($count / $options['limit']) : 1;
