bde4a8ea3bba1924e0c7b23b76466d55eb211e71
Raffael <github@raffaeljesche.de>
2020-02-09 22:48:45 +0100

    replace "cockpit()" with "$app->module()"

--- modules/Cockpit/views/accounts/account.php
+++ modules/Cockpit/views/accounts/account.php
@@ -112,7 +112,7 @@
                         <cp-actionbar>
                             <div class="uk-container uk-container-center">
                                 <button class="uk-button uk-button-large uk-button-primary">@lang('Save')</button>
-                                <a class="uk-button uk-button-large uk-button-link" href="@route(cockpit('cockpit')->hasaccess('cockpit', 'accounts') ? '/accounts' : '/')">@lang('Cancel')
+                                <a class="uk-button uk-button-large uk-button-link" href="@route($app->module('cockpit')->hasaccess('cockpit', 'accounts') ? '/accounts' : '/')">@lang('Cancel')
                                 </a>
                             </div>
                         </cp-actionbar>
