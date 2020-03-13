65e1ac9255826ddc14f7d565d21bff42a79c0d6d
Jonas Tang <jonas.u6c64@gmail.com>
2020-03-13 19:32:25 +0800

    add languange change filter

--- modules/Collections/views/entry.php
+++ modules/Collections/views/entry.php
@@ -5,6 +5,9 @@
     @if(isset($collection['color']) && $collection['color'])
     .app-header { border-top: 8px {{ $collection['color'] }} solid; }
     @endif
+    html.editor-en *:not(img){
+        filter: hue-rotate(90deg);
+    }
 </style>
 
 <script>
@@ -382,6 +385,8 @@
 
         persistLanguage(e) {
             App.session.set('collections.entry.'+this.collection._id+'.lang', e.target.value);
+            var lang = e.target.value
+            lang && document.querySelector('html').classList.add('editor-' + lang)
         }
 
         copyLocalizedValue(e) {
