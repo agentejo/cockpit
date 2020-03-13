deba7868c50ab621b5d91b14b1deb86fecb1c8ab
Jonas Tang <jonas.u6c64@gmail.com>
2020-03-13 20:45:56 +0800

    sb

--- modules/Collections/views/entry.php
+++ modules/Collections/views/entry.php
@@ -5,7 +5,7 @@
     @if(isset($collection['color']) && $collection['color'])
     .app-header { border-top: 8px {{ $collection['color'] }} solid; }
     @endif
-    html.editor-en *:not(img):not(canvas){
+    html.editor-en *:not(img):not(canvas):not(div):not(body):not(form){
         filter: hue-rotate(45deg);
     }
 </style>
