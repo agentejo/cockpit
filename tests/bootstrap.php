04b2e0cadc361ce48bbda79385e4a2aef2e8cd67
Raruto <Raruto@users.noreply.github.com>
2021-01-25 12:09:53 +0100

    add phpdocs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,17 @@
 <?php
+/**
+ * Make use of Cockpit autoloader to include it's own librararies.
+ *
+ * usage: open terminal and run { ./lib/vendor/bin/phpunit/ }
+ *
+ * @see https://phpunit.readthedocs.io/en/8.3/configuration.html#appendixes-configuration-phpunit-bootstrap
+ */
 
-declare(strict_types=1);
+/**
+ * @see { phpunit.xml.dist }
+ */
+if (!defined('COCKPIT_PHPUNIT') || !COCKPIT_PHPUNIT ) {
+    exit('Something goes wrong');
+}
 
 require_once __DIR__ . '/../bootstrap.php';
