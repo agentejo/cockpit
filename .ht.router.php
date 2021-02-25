01f323fe943be83e971325ea6f240f90e3112172
Raruto <Raruto@users.noreply.github.com>
2021-02-25 01:10:02 +0100

    add deny routes + composer "dev" script + improved rewrite engine

--- .ht.router.php
+++ .ht.router.php
@@ -17,31 +17,101 @@
  */
 if (PHP_SAPI == 'cli-server') {
 
-    $path  = pathinfo($_SERVER['SCRIPT_FILENAME']);
-    $index = realpath($path['dirname'].'/index.php');
+    // ----------------------------------------------------------------------
+    // File access
+    // ----------------------------------------------------------------------
+
+    // Deny access to application and system files from being viewed
+    if (preg_match('#(composer\.(json|lock)|package\.json|(README|CONTRIBUTING)\.md|cp|Dockerfile|LICENSE|\.(sqlite|sdb|s3db|db|yaml|yml))$#', $_SERVER['REQUEST_URI'])) {
+        header('HTTP/1.0 403 Forbidden');
+        exit('HTTP/1.0 403 Forbidden');
+    }
+
+    // ----------------------------------------------------------------------
+    // MIME Types
+    // ----------------------------------------------------------------------
+
+    /* handle static files (eg. assets/app/css/style.css) */
+
     $file  = $_SERVER['SCRIPT_FILENAME'];
+    $ext   = pathinfo($file, PATHINFO_EXTENSION);
 
-    /* "dot" routes (see: https://bugs.php.net/bug.php?id=61286) */
-    $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI'];
-
-    /* static files (eg. assets/app/css/style.css) */
-    if (is_file($file) && $path['extension'] != 'php') {
+    // Allow any files or directories that exist to be displayed directly
+    if (is_file($file)) {
 
         // custom Mime Types
-        if ($path['extension'] == 'tag') {
+        if ($ext == 'tag') {
             header('Content-Type: application/javascript');
             readfile($file);
             exit;
         }
 
-        // standard Mime Types
-        return false;
+        // default Mime Types.
+        if ($ext != 'php') {
+            return false;
+        }
 
     }
 
-    /* index files (eg. install/index.php) */
-    if (is_file($index)) {
-        include_once($index);
+    // ----------------------------------------------------------------------
+    // Rewrite Engine
+    // ----------------------------------------------------------------------
+
+    /* rewrite other URLs to index.php */
+
+    $htdocs = $_SERVER['DOCUMENT_ROOT'];
+    $path   = str_replace('/', DIRECTORY_SEPARATOR, parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    $index  = rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . 'index.php';
+    $folder = dirname($index);
+
+    if (is_file($htdocs . $path)) {
+
+        // Requested URI matches an existing "php" file
+        $index = $path;
+
+    } else {
+
+        // Trasverse directories and search for "index.php"
+        do {
+
+            $script = $folder . DIRECTORY_SEPARATOR . 'index.php';
+
+            if (is_file($htdocs . $script)) {
+                $index = $script;
+                break;
+            }
+
+            $folder = dirname($folder);
+
+        } while ($folder !== DIRECTORY_SEPARATOR && $folder !== '.');
+
     }
+
+    /* handle php files (eg. install/index.php) */
+
+     $index = DIRECTORY_SEPARATOR . ltrim($index, DIRECTORY_SEPARATOR);
+     $file  = $htdocs . $index;
+
+    // Update $_SERVER variables to point to the correct index-file.
+     $_SERVER['SCRIPT_FILENAME'] = $file;
+     $_SERVER['SCRIPT_NAME']     = $index;
+     $_SERVER['PHP_SELF']        = $index;
+
+    // Fix "dot" routes (see: https://bugs.php.net/bug.php?id=61286)
+     $_SERVER['PATH_INFO']       = $path;
+
+    // Deny access to files and directories whose names begin with a period
+    if (preg_match('#/\.|^\.(?!well-known/)#', $_SERVER['REQUEST_URI'])) {
+        header('HTTP/1.0 403 Forbidden');
+        exit('HTTP/1.0 403 Forbidden');
+    }
+
+    // Allow any "php" files that exist to be displayed directly
+    if (is_file($file) && dirname($index) != DIRECTORY_SEPARATOR) {
+        include_once $file;
+        exit;
+    }
+
+    /* Code execution returns to the main "index.php" file */
 
 }
